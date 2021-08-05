e4e6ff4164498b33ee2838dc05af913d8726c442
Shanker Karra <shankerkarra@gmail.com>
2021-08-05 15:39:26 -0600

    check in logo changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'docs',
-    sourcemap: false
-  },
-  server: {
-    port: 8080
-  }
-})
+  plugins: [vue()], npm i
