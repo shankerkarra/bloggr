d2eceb8bd2397eea359c5da33ce996ffa12cf37e
Shanker Karra <shankerkarra@gmail.com>
2021-08-05 16:48:58 -0600

    checkin bloggrThread

---
+++ src/components/BloggrThread.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <BloggrCard v-for="b in bloggrs" :key="b.id" :bloggr="b" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Thread',
+  props: {
+    bloggrs: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
