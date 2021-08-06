49daac09d1c5e581941bc9ed8fc8c26a6c04eff3
Shanker Karra <shankerkarra@gmail.com>
2021-08-05 22:21:05 -0600

    checkin

--- src/components/BloggrCard.vue
+++ src/components/BloggrCard.vue
@@ -4,7 +4,11 @@
       <img class="cover-img" :src="bloggr.imgUrl" alt="" srcset="">
       <div class="d-flex flex-grow-1 text-left">
         <h4 class="py-2 px-3">
-          {{ bloggr.title }}-{{ bloggr.body }}-{{ bloggr.imgUrl }}- {{ bloggr.tags }}-{{ bloggr.published }}-{{ bloggr.creatorId }}
+          Title: {{ bloggr.title }}<br>
+          Tags: {{ bloggr.tags }}<br>
+          Body: {{ bloggr.body }}<br>
+          Published: {{ bloggr.published }}
+          <!-- -{{ bloggr.creatorId }} -->
         </h4>
         <router-link router-link :to="{ name: 'Profile', params: {id: bloggr.creator.id } }" @click.stop="" class="creator p-3 align-self-end">
           <img class="h-100 rounded-pill" :src="bloggr.creator.picture" alt="" srcset="">
@@ -19,6 +23,7 @@
 import { computed } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import { bloggrsService } from '../services/BloggrsService'
+
 export default {
   props: {
     bloggr: {
